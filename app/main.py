a224e9832df645bbd680810f989ad5e0f525d119
Kailash Singh <kailash.singh@indixpert.com>
2025-10-08 03:15:18 +0530

    Testing without main -uvicorn call

--- app/main.py
+++ app/main.py
@@ -60,6 +60,6 @@ def GetStudentRegistration(studentid:str,email:str):
     except Exception as e:
         return {"ERROR":e}
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 8080))
+#     uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
---
+++ readme.md
@@ -0,0 +1,2 @@
+this is build pack not docker so look into this as well
+in this case you need to mention the port 
